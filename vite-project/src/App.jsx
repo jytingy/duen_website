61564389bbd25b6df26b273f2cc64172decd6d50
Jessica Ting <jyting@ucdavis.edu>
2025-10-18 00:06:14 -0700

    initial commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      DUEN Fall '25 Project Website
+      </div>
+    </>
+  )
+}
+
+export default App
