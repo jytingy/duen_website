58a4e0cabfaef5667d773d526217bb8b8a2befc7
Jessica Ting <jyting@ucdavis.edu>
2025-11-07 12:19:25 -0800

    deployment

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/duen_website/',  // 👈 important for GitHub Pages
   plugins: [react(), tailwindcss()],
 })
