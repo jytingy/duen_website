c8292e153eda1060aaa64911d87f82e0411920d7
Nancy Wang <nanwan@ucdavis.edu>
2025-11-13 21:24:06 -0800

    added lines to timeline

--- vite-project/src/components/Footer.jsx
+++ vite-project/src/components/Footer.jsx
@@ -3,9 +3,11 @@ function Footer() {
   return (
     <footer className="w-full bg-[#190A1D] text-white py-4">
       <div className="max-w-[1280px] mx-auto text-center">
-        <h3 className="text-md font-semibold mb-2 text-[#9a793f]">Questions? Email us at: </h3>
-        <h2 className="text-md font-semibold mb-2 text-[#E9D7AF]">nonchalanthustlersdavis@gmail.com</h2>
-        <h2 className="text-lg">Developed with 💜 by: Jessica & Nancy</h2>
+
+        <h2 className="text-md font-semibold mb-2 text-[#9a793f]">Questions? Email us at: <span className='text-[#E9D7AF]'>nonchalanthustlersdavis@gmail.com</span></h2>
+
+
+        <h3 className="text-md text-gray-200 font-semibold">Developed with 💜 by: <span className='text-[#e8d2fc]'>Jessica & Nancy</span></h3>
       </div>
     </footer>
   );
